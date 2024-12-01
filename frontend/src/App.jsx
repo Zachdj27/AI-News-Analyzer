d6ed88c26df0f63ad99c6f45d9553bbc7fc15c21
Zachary Djerdjouri <zdjer050@uottawa.ca>
2024-11-30 23:36:16 -0500

    changed api provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,36 +4,49 @@ import './App.css'
 
 
 function App() {
-  const [companyName, setCompanyName] = useState('');
+  const [tickerSymbol, setTickerSymbol] = useState('');
   const [summaries, setSummaries] = useState([]);
   const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState([]);
 
   const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
   const formatter = new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit'
   });
-  const formattedDate = formatter.format(today);
+  const formattedToday = formatter.format(today);
+  const formattedYesterday = formatter.format(yesterday);
 
   const fetchNews = async () => {
     //fetch news from api
-    if (!companyName.trim()) return;
+    if (!tickerSymbol.trim()) return;
 
-    console.log(import.meta.env.VITE_NEWS_API_KEY);
     const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+    const finnhub = require('finnhub');
 
-    // const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?' +
-    // `q=${companyName}&` +
-    // `from=${formattedDate}&` +
-    // 'sortBy=popularity&' +
-    // 'pageSize=5&' +
-    // `apiKey=${NEWS_API_KEY}`;
-    const NEWS_API_URL = `https://eodhd.com/api/news?s=AAPL.US&offset=0&limit=10&api_token=${NEWS_API_KEY}`
+    const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    api_key.apiKey = NEWS_API_KEY;
+    const finnhubClient = new finnhub.DefaultApi()
+
+
+    function getCompanyNews(symbol, from, to) {
+      return new Promise((resolve, reject) => {
+          finnhubClient.companyNews(symbol, from, to, (error, data, response) => {
+              if (error) {
+                  reject(error); 
+              } else {
+                  resolve(data); 
+              }
+          });
+      });
+    }
 
     try {
-      const response = await fetch(NEWS_API_URL);
+      const response = await getCompanyNews(tickerSymbol, formattedYesterday, formattedToday);
       const data = await response.json();
 
       if (data.articles && data.articles.length > 0) {
@@ -67,8 +80,8 @@ function App() {
         rows="2"
         cols="20"
         placeholder="Type Company Name"
-        value={companyName}
-        onChange={(e) => setCompanyName(e.target.value)}
+        value={tickerSymbol}
+        onChange={(e) => setTickerSymbol(e.target.value)}
         className="input-field"
       ></input>
       <br />
