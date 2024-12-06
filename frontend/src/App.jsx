1a388705e9d332c9abdb660d1f1c10f856ccb128
Zachary Djerdjouri <zdjer050@uottawa.ca>
2024-12-05 20:18:43 -0500

    modified request method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,52 +28,68 @@ function App() {
     const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
     const API_ENDPOINT = 'https://finnhub.io/api/v1';
 
-    const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-    api_key.apiKey = NEWS_API_KEY;
-    const finnhubClient = new finnhub.DefaultApi()
+    // const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    // api_key.apiKey = NEWS_API_KEY;
+    // const finnhubClient = new finnhub.DefaultApi()
 
 
-    function getCompanyNews(symbol, from, to) {
-      return new Promise((resolve, reject) => {
-          finnhubClient.companyNews(symbol, from, to, (error, data, response) => {
-              if (error) {
-                  reject(error); 
-              } else {
-                  resolve(data); 
-              }
-          });
-      });
-    }
+    // function getCompanyNews(symbol, from, to) {
+    //   return new Promise((resolve, reject) => {
+    //       finnhubClient.companyNews(symbol, from, to, (error, data, response) => {
+    //           if (error) {
+    //               reject(error); 
+    //           } else {
+    //               resolve(data); 
+    //           }
+    //       });
+    //   });
+    // }
 
     try {
       // const response = await getCompanyNews(tickerSymbol, formattedYesterday, formattedToday);
       //console.log(`https://finnhub.io/api/v1/company-news?symbol=${tickerSymbol}&from=${formattedYesterday}&to=${formattedToday}`);
-      // const response = await fetch(`${API_ENDPOINT}/company-news?symbol=APPL&from=2024-12-02&to=2024-12-03&token=${NEWS_API_KEY}`);
-      fetch(`${API_ENDPOINT}/company-news?symbol=APPL&from=2024-12-02&to=2024-12-03&token=${NEWS_API_KEY}`)
-        .then(response => response.json())
-        .then(data => console.log(data));
+      const response = await fetch(`${API_ENDPOINT}/company-news?symbol=AAPL&from=2024-12-02&to=2024-12-03&token=${NEWS_API_KEY}`);
+      // fetch(`${API_ENDPOINT}/company-news?symbol=AAPL&from=2024-12-02&to=2024-12-03&token=${NEWS_API_KEY}`)
+      //   .then(response => response.json())
+      //   .then(data => console.log(data));
 
 
-  //     const data = await response.json();
+      const data = await response.json();
 
-  //     if (data.articles && data.articles.length > 0) {
-  //       setArticles(data.articles);
+      // if (data.articles && data.articles.length > 0) {
+      //   setArticles(data.articles);
 
-  //       setLoading(true);
-  //       // process each article and get summaries
-  //       const articleSummaries = await Promise.all(
-  //         data.articles.map(async (article) => {
-  //           const articleContent = article.content || article.description || "No content available"; 
-  //           // const summary = await summarizeArticle(articleContent);
-  //           const summary = articleContent;
-  //           return summary;
-  //         })
-  //       );
-  //       setSummaries(articleSummaries);
-  //       setLoading(false);
-  //     } else {
-  //       console.log('No articles found.');
-  //     }
+      //   setLoading(true);
+      //   // process each article and get summaries
+      //   const articleSummaries = await Promise.all(
+      //     data.articles.map(async (article) => {
+      //       const articleContent = article.content || article.description || "No content available"; 
+      //       // const summary = await summarizeArticle(articleContent);
+      //       const summary = articleContent;
+      //       return summary;
+      //     })
+      //   );
+
+      setLoading(true);
+      const summaries = [];
+      const articles = [];
+      let i = 0;
+      console.log(data.length);
+      while (summaries.length < 6 && i < data.length){
+        const article = data[i];
+        const summary = article.summary;
+        if (summary.length > 0){
+          articles.push(article);
+          summaries.push(summary);
+        }
+        i++;
+      }
+      setArticles(articles);
+      setSummaries(summaries);
+      setLoading(false);
+      // } else {
+      //   console.log('No articles found.');
+      // }
     } catch (error) {
       console.error('Error fetching news:', error);
     }
